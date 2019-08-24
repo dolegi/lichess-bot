4c764461f20ec19166679b7bf56ac3c556370dd2
Daniel Dugovic <dandydand@gmail.com>
2019-08-24 13:21:53 -0500

    Add chat function for later use

--- request.go
+++ request.go
@@ -1,23 +1,42 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"log"
 	"net/http"
 )
 
 var client = &http.Client{}
 
-func request(method, path string) *http.Response {
-	req, err := http.NewRequest(method, conf.Url+path, nil)
+func chat(gameId, room, text string) *http.Response {
+	method := "POST"
+	path := "bot/game/"+gameId+"/chat"
+	line, _ := json.Marshal(map[string]string{"room": room, "text": text})
+	req, err := http.NewRequest(method, conf.Url+path, bytes.NewBuffer(line))
+	req.Header.Add("Authorization", "Bearer "+conf.Token)
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := client.Do(req)
 	if err != nil {
-		log.Println("Failed new request "+method+" "+path, err)
+		log.Println("Failed request "+method+" "+path, err)
 		return nil
 	}
+	if resp.StatusCode != http.StatusOK {
+		log.Printf("Response %d %s", resp.StatusCode, method+" "+path)
+	}
+	return resp
+}
+
+func request(method, path string) *http.Response {
+	req, err := http.NewRequest(method, conf.Url+path, nil)
 	req.Header.Add("Authorization", "Bearer "+conf.Token)
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Println("Failed authorized request "+method+" "+path, err)
+		log.Println("Failed request "+method+" "+path, err)
 		return nil
+	}
+	if resp.StatusCode != http.StatusOK {
+		log.Printf("Response %d %s", resp.StatusCode, method+" "+path)
 	}
 	return resp
 }
